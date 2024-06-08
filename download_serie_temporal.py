16ab170b95104d0a504e91f58ea3bfcaa9901e59
Jefferson Silva <jefferson.duarte@ifrn.edu.br>
2024-06-08 15:41:49 +0000

    adicionei suporte a mariadb

--- download_serie_temporal.py
+++ download_serie_temporal.py
@@ -7,38 +7,66 @@ import concurrent.futures
 import swifter
 import os
 
+import influxdb_client, os, time
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb import DataFrameClient
+
+
+
 pd.set_option('display.max_columns', None)  # or 1000
 pd.set_option('display.max_rows', None)  # or 1000
 pd.set_option('display.max_colwidth', None)  # or 199
 
+API_TOKEN_INFLUXDB = 'MEKovSzfY-UB99ZCVPun6A8GgntWK3sUWxKBEUHCvS8LeQYk8x_gd-OFlnlrLPWTv_sQAeHXn9um1xxKINLPyA=='
+
+token = '4QrxUJ8rO4lqJaDJytPxCPANBXt0Z3JSKOC3di3w43r5b5u_YSfD1QqHUqHnId98IozDiypUUn4HsSNKmI0i8w=='
+ORG = "TCC"
+URL = "http://localhost:8086"
+BUCKET = "series_temporais"
+
+
+#processos.append([tribunal, classe, data_ajuizamento, codigo, grau])
+    
+def save_pandas_to_infludb(df, measurement):
+    valores = df['numero_processo']
+    valores.index = df [['data_ajuizamento']]
+    tag = {'classe': df[['classe_codigo']], 'codigo': df[['codigo_orgaoJulgador']], 'grau': df[['grau']]}
+    client_from_pandas = DataFrameClient(host='127.0.0.1', port=8086, username='jefferson.silva', password='fcs!(%(Jeff))', database='series_temporais_tcc')
+    client_from_pandas.write_points(df, measurement, valores, tags = tag)
+    
+
+
 def converte_data(data_str):
-    return pd.to_datetime(data_str)
+    return pd.to_datetime(data_str).tz_convert('America/Sao_Paulo')
 
 
 def lista_para_dataframe(dados_dict):
   processos = []
   for processo in dados_dict['hits']['hits']:
     tribunal = processo['_source']['tribunal']
+    numero_processo = processo['_source']['numeroProcesso']
     grau = processo['_source']['grau']
     classe = processo['_source']['classe']['codigo']
     data_ajuizamento = processo['_source']['dataAjuizamento']
     codigo = processo['_source']['orgaoJulgador']['codigo']
 
-    processos.append([tribunal, classe, data_ajuizamento, codigo, grau])
+    processos.append([tribunal, processo, classe, data_ajuizamento, codigo, grau])
 
-  df = pd.DataFrame(processos, columns=['tribunal','classe_codigo', 'data_ajuizamento', 'codigo_orgaoJulgador', 'grau'])
-  #df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(converte_data)
-  df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(dateutil.parser.parse)
-  df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(datetime.datetime.date)
+  df = pd.DataFrame(processos, columns=['tribunal','numero_processo','classe_codigo', 'data_ajuizamento', 'codigo_orgaoJulgador', 'grau'])
+  df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(converte_data)
+  #df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(dateutil.parser.parse)
+  #df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(datetime.datetime.date)
    
   return df
 
 df_tribunal = pd.DataFrame()
-url = "https://api-publica.datajud.cnj.jus.br/api_publica_tjmg/_search"
+url = "https://api-publica.datajud.cnj.jus.br/api_publica_tjce/_search"
 api_key = "APIKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==" # Chave pública
-tribunal = 'TJMG'
-size = 10000
+tribunal = 'TJCE'
+size = 1000
 data = '2018-01-01'
+
 
 payload = json.dumps(
 {
@@ -62,6 +90,8 @@ headers = {
 response = requests.request("POST", url, headers=headers, data=payload)  # <Response [200]>
 dados_dict = response.json() # <class 'dict'>
 df_tribunal = lista_para_dataframe(dados_dict)
+#df_tribunal.set_index("data_ajuizamento", inplace=True)
+save_pandas_to_infludb(df_tribunal, tribunal)
 numero_processos = size
 
 while numero_processos == size:
@@ -97,20 +127,26 @@ while numero_processos == size:
     dados_dict = response.json() # <class 'dict'>
     numero_processos = len(dados_dict['hits']['hits'])
     ultima_posicao_dicionario = dados_dict['hits']['hits'][(len(dados_dict['hits']['hits'])-1)]['sort']
-    df_tribunal = pd.concat([df_tribunal, lista_para_dataframe(dados_dict)])
-    #print(df_tribunal.head(2))
-    #print(df_tribunal.tail(1))
+    df_tribunal = lista_para_dataframe(dados_dict)
+    df_tribunal.set_index("data_ajuizamento", inplace=True)
+    save_pandas_to_infludb(df_tribunal, tribunal)
+
+    
+    #df_tribunal = pd.concat([df_tribunal, lista_para_dataframe(dados_dict)])
+
     tamanho_dataset = len(df_tribunal.index)
     ultima_data_ajuizamento = dados_dict['hits']['hits'][len(dados_dict['hits']['hits'])-1]['_source']['dataAjuizamento']
     print(f'{datetime.datetime.now()}\t Número de processos: {tamanho_dataset} \t Data do último processo adicionado: {ultima_data_ajuizamento}' )
     #if tamanho_dataset > 2000000:
     #  break
 
-df_tribunal.to_csv('dados/processados/serie_temporal_ajuizamento-TJMG_2018.csv', header=True, sep=';', compression='zip')
+df_tribunal.to_csv('dados/processados/serie_temporal_ajuizamento-TJCE_2018.csv', header=True, sep=';', compression='zip')
 df_g1 = df_tribunal[df_tribunal['grau'] == 'G1'] 
-df_g1.to_csv('dados/processados/serie_temporal_ajuizamento-TJMG_G1_2018.csv', header=True, sep=';', compression='zip')
+df_g1.to_csv('dados/processados/serie_temporal_ajuizamento-TJCE_G1_2018.csv', header=True, sep=';', compression='zip')
 df_g2 = df_tribunal[df_tribunal['grau'] == 'G2'] 
-df_g2.to_csv('dados/processados/serie_temporal_ajuizamento-TJMG_G2_2018.csv', header=True, sep=';', compression='zip')
+df_g2.to_csv('dados/processados/serie_temporal_ajuizamento-TJCE_G2_2018.csv', header=True, sep=';', compression='zip')
 df_je = df_tribunal[df_tribunal['grau'] == 'JE'] 
-df_je.to_csv('dados/processados/serie_temporal_ajuizamento-TJMG_JE_2018.csv', header=True, sep=';', compression='zip')
+df_je.to_csv('dados/processados/serie_temporal_ajuizamento-TJCE_JE_2018.csv', header=True, sep=';', compression='zip')
+write_api.__del__()
+client.__del__()
 print(df_tribunal.head(1))
--- teste_2.py
+++ teste_2.py
@@ -5,13 +5,14 @@ import datetime
 import concurrent.futures
 import swifter
 import os
+import dateutil
 
-pd.set_option('display.max_columns', None)  # or 1000
-pd.set_option('display.max_rows', None)  # or 1000
-pd.set_option('display.max_colwidth', None)  # or 199
+
 
 def converte_data(data_str):
-    return pd.to_datetime(data_str).tz_convert('America/Sao_Paulo')
+    data = dateutil.parser.parse(data_str)
+    data = datetime.datetime.date(data)
+    return data
 
 
 def gera_lista_assuntos(assuntos_do_df):
@@ -63,41 +64,38 @@ def lista_para_dataframe(dados_dict):
     except:
       assuntos = []
     data_ajuizamento = processo['_source']['dataAjuizamento']
-    ultima_atualizacao = processo['_source']['dataHoraUltimaAtualizacao']
     #formato = processo['_source']['formato']['nome']
     codigo = processo['_source']['orgaoJulgador']['codigo']
     orgao_julgador = processo['_source']['orgaoJulgador']['nome']
     municipio = processo['_source']['orgaoJulgador']['codigoMunicipioIBGE']
     sort = processo['sort'][0]
     try:
-      movimentos = processo['_source']['movimentos']
+        movimentos = processo['_source']['movimentos']
     except:
       movimentos = []
 
-    processos.append([numero_processo, classe, data_ajuizamento, ultima_atualizacao, \
+    processos.append([numero_processo, classe, data_ajuizamento,
                       codigo, orgao_julgador, municipio, grau, assuntos, movimentos, sort])
 
-  df = pd.DataFrame(processos, columns=['numero_processo', 'classe', 'data_ajuizamento', 'ultima_atualizacao', \
-                        'codigo', 'orgao_julgador', 'municipio', 'grau', 'assuntos', 'movimentos', 'sort'])
+  df = pd.DataFrame(processos, columns=['numero_processo', 'classe', 'data_ajuizamento',
+                                        'codigo', 'orgao_julgador', 'municipio', 'grau', 'assuntos', 'movimentos', 'sort'])
   df['movimentos'] = df['movimentos'].swifter.apply(gera_lista_movimentos_multithread)
   df['assuntos'] = df['assuntos'].swifter.apply(gera_lista_assuntos)
   #df['movimentos'] = df['movimentos'].swifter.apply(gera_lista_movimentos_multithread)
   df['data_ajuizamento'] = df['data_ajuizamento'].swifter.apply(converte_data)
-  df['ultima_atualizacao'] = df['ultima_atualizacao'].swifter.apply(converte_data)
   try:
     df['movimentos']= df['movimentos'].swifter.apply(lambda x: sorted(x, key=lambda tup: tup[2], reverse=False))
   except:
     pass
-  #print(df.head(1))  
   return df
 
 df_tribunal = pd.DataFrame()
-url = "https://api-publica.datajud.cnj.jus.br/api_publica_tjrn/_search"
+url = "https://api-publica.datajud.cnj.jus.br/api_publica_tjce/_search"
 api_key = "APIKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw==" # Chave pública
-tribunal = 'TJRN'
+tribunal = 'TJCE'
 orgaoJulgador = '4º JUIZADO ESPECIAL DA FAZENDA PÚBLICA'
-size = 2500
-data = '2024-02-01'
+size = 5000
+data = '2019-01-01'
 
 payload = json.dumps(
 {
@@ -106,7 +104,6 @@ payload = json.dumps(
     "bool": {
     "must": [
         {"match": {"tribunal": tribunal}},
-        {"match": {"orgaoJulgador.nome": orgaoJulgador}},
         {"range": {"dataAjuizamento": {"gte": data }}}
     ]
     }
@@ -139,7 +136,6 @@ while numero_processos == size:
         "bool": {
             "must": [
             {"match": {"tribunal": tribunal}},
-            {"match": {"orgaoJulgador.nome": orgaoJulgador}},
             {"range": {"dataAjuizamento": {"gte": data}}}
             
             ]
@@ -158,13 +154,12 @@ while numero_processos == size:
     dados_dict = response.json() # <class 'dict'>
     numero_processos = len(dados_dict['hits']['hits'])
     ultima_posicao_dicionario = dados_dict['hits']['hits'][(len(dados_dict['hits']['hits'])-1)]['sort']
+    
     df_tribunal = pd.concat([df_tribunal, lista_para_dataframe(dados_dict)])
-    #print(df_tribunal.head(2))
-    df_tribunal = df_tribunal[df_tribunal['orgao_julgador'] == orgaoJulgador]
-    print(df_tribunal.head(1))
+    print(df_tribunal.tail(1))
     tamanho_dataset = len(df_tribunal.index)
     ultima_data_ajuizamento = dados_dict['hits']['hits'][len(dados_dict['hits']['hits'])-1]['_source']['dataAjuizamento']
-    print(f'{datetime.datetime.now()}\t Serventia: {orgaoJulgador}\tNúmero de processos: {tamanho_dataset} \t Data do último processo adicionado: {ultima_data_ajuizamento}' )
+    print(f'{datetime.datetime.now()}\tNúmero de processos: {tamanho_dataset} \t Data do último processo adicionado: {ultima_data_ajuizamento}' )
     #if tamanho_dataset > 2000000:
     #  break
 
@@ -173,3 +168,5 @@ try:
 except:
     print(f'Última página do dicionário veio vazia: {orgaoJulgador}')
 print(df_tribunal.head(1))
+
+df_tribunal.to_csv(f'dados/{tribunal}_{data}.csv', index=False, header=True, sep=';')
